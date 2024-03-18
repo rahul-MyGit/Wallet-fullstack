09b81f9dd1627521c84acb179938c6505b2c351f
Rahul <rahulhero93111@gmail.com>
2024-03-18 23:48:49 +0530

    added Signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import SendMoney from "./pages/SendMoney"
+import Dashboard from "./pages/Dashboard"
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className='bg-green-400'>hi there</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
+
+
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,28 @@
+import { Heading } from "../components/Heading"
+import { SubHeading } from "../components/SubHeading";
+import { InputBox } from "../components/InputBox";
+import { Button } from "../components/Button";
+import { BottomWarning } from "../components/BottomWarning";
+
+
+export const Signup = ()=>{
+  return <div className="bg-slate-300 h-screen flex justify-center">
+    <div className="flex flex-col justify-center">
+      <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+        <Heading label={"Sign up"} />
+        <SubHeading label={"Enter your  details to create an account."} />
+        <InputBox placeholder="Rahul" label={"First Name"}/>
+        <InputBox placeholder="Gujjar" label={"Last Name"}/>
+        <InputBox placeholder="rahul@gmail.com" label={"Email"}/>
+        <InputBox placeholder="12345" label={"Password"}/>
+        <div className="pt-4">
+          <Button label={"Submit"} />
+        </div>
+        <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
+
+      </div>
+    </div>
+  </div>
+}
+
+export default Signup
